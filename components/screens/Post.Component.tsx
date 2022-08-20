02f5a83c519bcb88481c4973c6353c2723352243
Yuen Ler Chow <yuenlerchow@college.harvard.edu>
2022-08-19 20:23:56 -0400

    make tsx

--- components/screens/Post.Component.tsx
+++ components/screens/Post.Component.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useState } from 'react';
 import { View } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import { ListItem, Icon } from '@rneui/themed';
